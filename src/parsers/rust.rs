ad2b10fc1b05aea6b196a711b4f7938bbe253128
Nevin1901 <nevinpuri1901@gmail.com>
2022-05-13 19:43:12 +0200

    added impl

--- src/parsers/rust.rs
+++ src/parsers/rust.rs
@@ -3,7 +3,7 @@ use std::vec::Vec;
 use crate::parsers::{
     types::{CommentType, SymbolPosition, Span, SymbolSpan, LineSpan},
     base::Parser};
-use syn::{Attribute, Expr, Result, File, ItemFn, Item, visit::{self, Visit}};
+use syn::{Attribute, Expr, Result, File, ItemFn, Item, ImplItem, visit::{self, Visit}};
 use syn::spanned::Spanned;
 
 // use syn::__private::ToTokens;
@@ -14,8 +14,23 @@ struct FnVisitor {
     symbols: Vec<(SymbolSpan, SymbolSpan)>,
 }
 
+// will try to get these merged into upstream syn so this code doesn't have to be here.
+// Also if you have an idea of how I can walk the entire syntax tree without repetitive code all
+// help is welcome
+pub fn get_attrs_impl_item(item: &ImplItem) -> Option<&Vec<Attribute>> {
+    match item {
+        ImplItem::Const(syn::ImplItemConst { attrs, ..})
+            | ImplItem::Method(syn::ImplItemMethod { attrs, ..})
+            | ImplItem::Type(syn::ImplItemType { attrs, .. })
+            | ImplItem::Macro(syn::ImplItemMacro { attrs, .. }) => {
+                Some(attrs)
+            },
+            _ => None
+    }
+}
+
 /// Returns a shared reference to the attributes of the item.
-pub fn get_attrs(item: &Item) -> Option<&Vec<Attribute>> {
+pub fn get_attrs_item(item: &Item) -> Option<&Vec<Attribute>> {
     match item {
         Item::Const(syn::ItemConst { attrs, .. })
         | Item::Enum(syn::ItemEnum { attrs, .. })
@@ -32,7 +47,6 @@ pub fn get_attrs(item: &Item) -> Option<&Vec<Attribute>> {
         | Item::TraitAlias(syn::ItemTraitAlias { attrs, .. })
         | Item::Type(syn::ItemType { attrs, .. })
         | Item::Union(syn::ItemUnion { attrs, .. }) => {
-            println!("ok");
             Some(attrs)
         },
         _ => None
@@ -41,28 +55,12 @@ pub fn get_attrs(item: &Item) -> Option<&Vec<Attribute>> {
 
 impl<'ast> Visit<'ast> for FnVisitor {
     fn visit_item(&mut self, node: &'ast Item) {
-        match node {
-            Item::Fn(item) => {
+        println!("{:#?}", node);
 
-                let span = item.span();
-                let attrs = &item.attrs;
+        let fun = node.span();
+        // println!("{:#?}", span);
 
-                // return item, get attr, get span from item, return both
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-                println!("GOT ITEM SPAN");
-            },
-            _ => {
-            }
-        }
-        let attrs = match get_attrs(node) {
+        let attrs = match get_attrs_item(node) {
             Some(attrs) => attrs,
             _ => {
                 return visit::visit_item(self, node);
@@ -76,12 +74,57 @@ impl<'ast> Visit<'ast> for FnVisitor {
             }
         };
 
-        // and then subtract the span
-        // println!("{:#?}", item.span());
-        // println!("{:#?}", attrs);
-        // let mut tokens = quote!(#node);
-        // println!("{:#?}", tokens);
+        let function = SymbolSpan {
+            start: LineSpan {
+                line: comment.end.line + 1,
+                character: fun.start().column
+            },
+            end: LineSpan {
+                line: fun.end().line,
+                character: fun.end().column
+            }
+        };
+
+        println!("{:#?}", comment);
+        println!("{:#?}", function);
+
+        self.symbols.push((comment, function));
+
         return visit::visit_item(self, node);
+    }
+
+    fn visit_impl_item(&mut self, node: &'ast ImplItem) {
+        let fun = node.span();
+
+        let attrs = match get_attrs_impl_item(node) {
+            Some(attrs) => attrs,
+            _ => {
+                return visit::visit_impl_item(self, node);
+            }
+        };
+
+        let comment = match get_comment_range(attrs) {
+            Some(comment) => comment,
+            _ => {
+                return visit::visit_impl_item(self, node);
+            }
+        };
+
+        let function = SymbolSpan {
+            start: LineSpan {
+                line: comment.end.line + 1,
+                character: fun.start().column
+            },
+            end: LineSpan {
+                line: fun.end().line,
+                character: fun.end().column
+            }
+        };
+
+        println!("{:#?}", comment);
+        println!("{:#?}", function);
+
+        self.symbols.push((comment, function));
     }
 }
 
@@ -89,7 +132,7 @@ impl Parser for RsParser {
     // essentially check the attr for comment, and then check the span of the item.
     #[proc_macro_derive(MyMacro)]
     fn parse(&self, file_input: &str) -> Vec<(SymbolSpan, SymbolSpan)> {
-        let mut vec = Vec::new();
+        // let mut vec = Vec::new();
         println!("using rust parser");
         let ast = syn::parse_file(file_input).unwrap();
         let mut visitor = FnVisitor { symbols: Vec::new() };
@@ -97,100 +140,8 @@ impl Parser for RsParser {
 
         println!("{}", visitor.symbols.len());
 
-        for attr in &ast.attrs {
-        }
-
-        for item in &ast.items {
-            match item {
-                syn::Item::Fn(expr) => {
-                    match parseFun(expr) {
-                        Some(symbol) => {
-                            vec.push(symbol);
-                        },
-                        _ => {
-                            println!("no symbol")
-                        }
-                    };
-                },
-                syn::Item::Impl(expr) => {
-                    let mut symbols = parseImpl(&expr);
-                    vec.append(&mut symbols);
-                    // println!("impl");
-                }
-                _ => {
-                    // println!("none");
-                }
-            }
-        }
-
-        /*
-        for symbol in &vec {
-            println!("{:#?}", symbol);
-        }
-        */
-
-        return vec;
+        return visitor.symbols;
     }
-}
-
-fn parseImpl(expr: &syn::ItemImpl) -> Vec<(SymbolSpan, SymbolSpan)> {
-    let mut vec: Vec<(SymbolSpan, SymbolSpan)> = Vec::new();
-    if expr.attrs.len() > 0 {
-        match get_comment_range(&expr.attrs) {
-            Some(comment) => {
-
-                let fun = expr.span();
-
-                let function = SymbolSpan {
-                    start: LineSpan {
-                        line: fun.start().line,
-                        character: fun.start().column
-                    },
-                    end: LineSpan {
-                        line: fun.end().line,
-                        character: fun.end().column
-                    }
-                };
-
-                vec.push((comment, function));
-            },
-            _ => {
-            }
-        }
-        // check for comment above or in impl, and add that into the vec
-    }
-
-    for item in &expr.items {
-        match item {
-            // now it's basically a function
-            syn::ImplItem::Method(method) => {
-                let comment = match get_comment_range(&method.attrs) {
-                    Some(comment) => comment,
-                    _ => {
-                        continue;
-                    }
-                };
-
-                let fun = method.block.span();
-
-                let function = SymbolSpan {
-                    start: LineSpan {
-                        line: fun.start().line,
-                        character: fun.start().column
-                    },
-                    end: LineSpan {
-                        line: fun.end().line,
-                        character: fun.end().column
-                    }
-                };
-
-                vec.push((comment, function))
-            },
-            _ => {}
-        }
-    }
-
-    return vec;
 }
 
 fn get_comment_range(attrs: &Vec<syn::Attribute>) -> Option<SymbolSpan> {
@@ -235,41 +186,3 @@ fn get_comment_range(attrs: &Vec<syn::Attribute>) -> Option<SymbolSpan> {
     });
 }
 
-fn parseFun(expr: &syn::ItemFn) -> Option<(SymbolSpan, SymbolSpan)> {
-    if expr.attrs.len() <= 0 {
-        return None;
-    }
-
-    let comment = match get_comment_range(&expr.attrs) {
-        Some(comment) => comment,
-        _ => {
-            return None;
-        }
-    };
-
-    let fun = expr.block.span();
-
-    let function = SymbolSpan {
-        start: LineSpan {
-            line: fun.start().line,
-            character: fun.start().column
-        },
-        end: {
-            LineSpan {
-                line: fun.end().line,
-                character: fun.end().column
-            }
-        }
-    };
-
-    // println!("{:#?}", comment);
-    // println!("{:#?}", function);
-    return Some((comment, function));
-    // println!("{:#?}", expr.block.span().start().line);
-    // println!("{:#?}", expr.block.span().end().line);
-    // println!("{:#?}", expr.attrs[0].path.segments[0].ident.to_string());
-    // println!("{:#?}", expr);
-    // println!("{:#?}", expr.span().start());
-    // println!("{:#?}", expr.span().end());
-    // println!("fun");
-}
