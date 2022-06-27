1be206fd28af09ada232fcf3d81070c1e167502d
Nevin1901 <me@nevin.cc>
2022-06-27 20:37:34 +0200

    parser rules

--- src/parsers/javascript/comment_parser.rs
+++ src/parsers/javascript/comment_parser.rs
@@ -1,23 +1,49 @@
 use nom::{
-    sequence::preceded, IResult, bytes::complete::{take_until, tag},
+    sequence::{preceded, tuple}, IResult, bytes::complete::{take_until, tag},
 };
-use nom_locate::LocatedSpan;
+use nom_locate::{LocatedSpan, position};
+
+use crate::parsers::types::{SymbolSpan, LineSpan};
 
 pub type NomSpan<'a> = LocatedSpan<&'a str>;
 
 fn start(input: NomSpan) -> IResult<NomSpan, NomSpan> {
-    preceded(take_until("/*"), tag("/*"))(input)
+    let (input, start) = preceded(take_until("/*"), tag("/*"))(input)?;
+    let (input, pos) = position(input)?;
+    Ok((input, pos))
 }
 
 pub fn body(input: NomSpan) -> IResult<NomSpan, NomSpan> {
-    take_until("*/")(input)
+    let (input, body) = take_until("*/")(input)?;
+    let (input, pos) = position(input)?;
+    Ok((input, pos))
 }
 pub fn end(input: NomSpan) -> IResult<NomSpan, NomSpan> {
-    tag("*/")(input)
+    let (input, end) = tag("*/")(input)?;
+    let (input, pos) = position(input)?;
+    Ok((input, pos))
 }
 
-pub fn get_comment(input: NomSpan) -> IResult<NomSpan, NomSpan> {
-    panic!("Not implemented");
+pub fn get_comment(input: NomSpan) -> IResult<NomSpan, SymbolSpan> {
+    let (input, (start, body, end)) = tuple((start, body, end))(input)?;
+    let start_line = start.location_line();
+    let start_char = start.get_column();
+
+    let end_line = end.location_line();
+    let end_char = end.get_column();
+
+    let symbol = SymbolSpan {
+        start: LineSpan {
+            line: usize::try_from(start_line).unwrap(),
+            character: start_char
+        },
+        end: LineSpan {
+            line: usize::try_from(end_line).unwrap(),
+            character: end_char
+        }
+    };
+
+    Ok((input, symbol))
 }
 
 pub fn parse_comments(text: &str) {
