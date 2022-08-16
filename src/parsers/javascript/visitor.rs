7967abefad6bd73862298e603647e768c56daa61
Nevin1901 <nevinpuri1901@gmail.com>
2022-08-16 21:44:07 +0200

    updated js visitor

--- src/parsers/javascript/visitor.rs
+++ src/parsers/javascript/visitor.rs
@@ -41,6 +41,60 @@ impl<'a> Visit for JsVisitor<'a> {
         self.symbols.push(to_symbol_span(&self.text, span.lo.0, span.hi.0));
     }
 
+    fn visit_expr(&mut self, expr: &swc_ecma_ast::Expr) {
+        let span = match expr {
+            swc_ecma_ast::Expr::This(e) => e.span,
+            swc_ecma_ast::Expr::Array(e) => e.span,
+            swc_ecma_ast::Expr::Object(e) => e.span,
+            swc_ecma_ast::Expr::Fn(e) => e.function.span,
+            swc_ecma_ast::Expr::Unary(e) => e.span,
+            swc_ecma_ast::Expr::Update(e) => e.span,
+            swc_ecma_ast::Expr::Bin(e) => e.span,
+            swc_ecma_ast::Expr::Assign(e) => e.span,
+            swc_ecma_ast::Expr::Member(e) => e.span,
+            swc_ecma_ast::Expr::SuperProp(e) => e.span,
+            swc_ecma_ast::Expr::Cond(e) => e.span,
+            swc_ecma_ast::Expr::Call(e) => e.span,
+            swc_ecma_ast::Expr::New(e) => e.span,
+            swc_ecma_ast::Expr::Seq(e) => e.span,
+            swc_ecma_ast::Expr::Ident(e) => e.span,
+            swc_ecma_ast::Expr::Tpl(e) => e.span,
+            swc_ecma_ast::Expr::TaggedTpl(e) => e.span,
+            swc_ecma_ast::Expr::Arrow(e) => e.span,
+            swc_ecma_ast::Expr::Class(e) => e.class.span,
+            swc_ecma_ast::Expr::Yield(e) => e.span,
+            swc_ecma_ast::Expr::MetaProp(e) => e.span,
+            swc_ecma_ast::Expr::Await(e) => e.span,
+            swc_ecma_ast::Expr::Paren(e) => e.span,
+            swc_ecma_ast::Expr::JSXMember(e) => e.prop.span,
+            swc_ecma_ast::Expr::JSXNamespacedName(e) => e.ns.span,
+            swc_ecma_ast::Expr::JSXEmpty(e) => e.span,
+            swc_ecma_ast::Expr::JSXElement(e) => e.span,
+            swc_ecma_ast::Expr::JSXFragment(e) => e.span,
+            swc_ecma_ast::Expr::TsTypeAssertion(e) => e.span,
+            swc_ecma_ast::Expr::TsConstAssertion(e) => e.span,
+            swc_ecma_ast::Expr::TsNonNull(e) => e.span,
+            swc_ecma_ast::Expr::TsAs(e) => e.span,
+            swc_ecma_ast::Expr::TsInstantiation(e) => e.span,
+            swc_ecma_ast::Expr::PrivateName(e) => e.span,
+            swc_ecma_ast::Expr::OptChain(e) => e.span,
+            swc_ecma_ast::Expr::Invalid(e) => e.span,
+            swc_ecma_ast::Expr::Lit(e) => {
+                match e {
+                    swc_ecma_ast::Lit::Str(n) => n.span,
+                    swc_ecma_ast::Lit::Bool(n) => n.span,
+                    swc_ecma_ast::Lit::Null(n) => n.span,
+                    swc_ecma_ast::Lit::Num(n) => n.span,
+                    swc_ecma_ast::Lit::BigInt(n) => n.span,
+                    swc_ecma_ast::Lit::Regex(n) => n.span,
+                    swc_ecma_ast::Lit::JSXText(n) => n.span,
+                }
+            },
+        };
+
+        self.symbols.push(to_symbol_span(&self.text, span.lo.0, span.hi.0));
+    }
+
     fn visit_class_member(&mut self, member: &swc_ecma_ast::ClassMember) {
         let span = match member {
             swc_ecma_ast::ClassMember::Constructor(ref e) => e.span,
